8668bc837f7533b7f4c343da88f285fab6a9552f
Jing Wang <jingwangw12@gmail.com>
2025-04-13 12:45:39 -0400

    added a shoe box container outside of the shoe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,20 +4,28 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Sneaker Classifier</h1>
+      {/* the top for the shoe box: brown trapezoid*/}
+      <div className="shoe-cap">
+        {/* the small black rectangle */}
+        <div className="small-trap"></div>
+        
+        <h1 className="title">Sneaker Classifier</h1>
 
-      {/* the container for the shoe */}
-      <div className="image-container">
-        <img
-          className="left-shoe"
-          src="/src/assets/orange-blue-shoe-left.png"
-          alt="left shoe image"
-        />
-        <img
-          className="right-shoe"
-          src="/src/assets/orange-blue-shoe-right.png"
-          alt="right shoe image"
-        />
+        {/* Container of the shoe */}
+        <div className="outer-shoe-container">
+          <div className="inner-shoe-container">
+            <img
+              className="left-shoe"
+              src="/src/assets/orange-blue-shoe-left.png"
+              alt="left shoe image"
+            />
+            <img
+              className="right-shoe"
+              src="/src/assets/orange-blue-shoe-right.png"
+              alt="right shoe image"
+            />
+          </div>
+        </div>
       </div>
     </>
   );
