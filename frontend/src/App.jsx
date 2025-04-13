f2a8680396a563dfb519ac3c830be1beafddad2d
Jing Wang <jingwangw12@gmail.com>
2025-04-13 13:18:58 -0400

    added input box for image and output container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,22 +8,30 @@ function App() {
       <div className="shoe-cap">
         {/* the small black rectangle */}
         <div className="small-trap"></div>
-        
+
         <h1 className="title">Sneaker Classifier</h1>
 
-        {/* Container of the shoe */}
+        {/* outter box of the shoe */}
         <div className="outer-shoe-container">
+          {/* inner box of the shoe */}
           <div className="inner-shoe-container">
             <img
               className="left-shoe"
               src="/src/assets/orange-blue-shoe-left.png"
               alt="left shoe image"
             />
+
+            {/* The input box for image */}
+            <input type="file" className="img-input" accept="image/*" />
+
             <img
               className="right-shoe"
               src="/src/assets/orange-blue-shoe-right.png"
               alt="right shoe image"
             />
+            <div className="output">
+              <h3>Output</h3>
+            </div>
           </div>
         </div>
       </div>
